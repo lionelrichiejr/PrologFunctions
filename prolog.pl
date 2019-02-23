bc4cf56abc64538356297af03f7c8994c2d192da
Gaylon Robinson <robinson.gaylon13@gmail.com>
2019-02-22 23:39:26 -0500

    Added second case of sum-up-numbers-simple. If an element of the list is a number then it is added to the sum.

--- prolog.pl
+++ prolog.pl
@@ -1,1 +1,7 @@
 sum-up-numbers-simple([],0). % Case 0: sum is 0 when list is empty.
+sum-up-numbers-simple(L,N):- % Case 1: first element is a number.
+    L = [A|B], % check if first element in list is a number.
+    number(A), 
+    \+ (B=[]), % check if rest of list is not empty.
+    sum-up-numbers-simple(B,N1), % compute sum of the rest of the list.
+    N is A + N1. % sum is first element plus the sum of the remainder of the list.
